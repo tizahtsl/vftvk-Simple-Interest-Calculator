0394213468375595910b974ca89a5b8117da8c58
Adwoa <tizah.tsl@gmail.com>
2021-08-15 17:14:38 -0400

    style

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate/100;
     var year = new Date().getFullYear() + parseInt(years);
-    document.getElementById("result").innerHTML = "if you deposit " + "<span style='color: red;'>" + principal +"</span>"+ ", \<br\> at an interest rate of " + "<span style='color: red;'>" + rate + "%"
-+"</span>"+ "  \<br\>You will recieve an amount of " + "<span style='color: red;'>" + interest + "</span>" + ", \<br\> in the year " + "<span style='color: red;'>"+ year + "</span>" +", \<br\>" 
+    document.getElementById("result").innerHTML = "if you deposit " + "<span style='background-color: yellow;'>" + principal +"</span>"+ ", \<br\> at an interest rate of " + "<span style='background-color: yellow;'>" + rate + "%"
++"</span>"+ "  \<br\>You will recieve an amount of " + "<span style='background-color: yellow;>" + interest + "</span>" + ", \<br\> in the year " + "<span style='background-color: yellow;'>"+ year + "</span>" +", \<br\>" 
  
  
 }
