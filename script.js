9e1a0b2c72884aedbe587c4b8240301f027fdfbc
Adwoa <tizah.tsl@gmail.com>
2021-08-15 17:43:58 -0400

    style

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function compute()
     var interest = principal * years * rate/100;
     var year = new Date().getFullYear() + parseInt(years);
     document.getElementById("result").innerHTML = "If you deposit " + "<span style='background-color: yellow;'>" + principal +"</span>"+ ", \<br\> at an interest rate of " + "<span style='background-color: yellow;'>" + rate + "%"
-+"</span>"+ "  \<br\>You will recieve an amount of " + "<span style='background-color: yellow;>" + interest + ",</span>" + " ,\<br\> in the year " + "<span style='background-color: yellow;'>"+ year + "</span>" +". \<br\>" 
++"</span>"+ "  \<br\>You will recieve an amount of " + "<span style='background-color: yellow;>" + interest + "</span>" + ",\<br\> in the year " + "<span style='background-color: yellow;'>"+ year + "</span>" +". \<br\>" 
  
  
 }
