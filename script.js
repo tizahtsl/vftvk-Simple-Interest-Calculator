870b41cfb590cab32d01ea6f31e41565357ac247
Adwoa <tizah.tsl@gmail.com>
2021-08-15 16:28:40 -0400

    Adding

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
     
+    var principal  = document.getElementById("principal").value;
+    if (principal < 1){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+}
+  
+    
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate/100;
+    var year = new Date().getFullYear() + parseInt(years);
+    document.getElementById("result").innerHTML = "if you deposit " + "<span style='color: red;'>" + principal +"</span>"+ ", \<br\> at an interest rate of " + "<span style='color: red;'>" + rate + "%"
++"</span>"+ "  \<br\>You will recieve an amount of " + "<span style='color: red;'>" + interest + "</span>" + ", \<br\> in the year " + "<span style='color: red;'>"+ year + "</span>" +", \<br\>" 
+ 
+ 
+}
+
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
